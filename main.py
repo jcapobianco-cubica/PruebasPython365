e23e1b30bc802f29e6c0e70c98792f8ba1ff45b1
Junior Capobianco <jcapobianco@cubica.cloud>
2024-03-26 17:25:50 +0100

    agregada clase para envio de correo

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import secrets
+import Mail
 import Sharepoint
 import asyncio
 
 asyncio.run(Sharepoint.get_users())
+
+async def main():
+    naar = ["recipient@contoso.com"]
+    cc = []
+    bcc = []
+
+    mailbericht = Mail.create_mail(Mail.html, "Test Email Subject", naar, cc, bcc)
+
+    # Create a list of tasks to run concurrently
+    tasks = [Mail.send_mail(mailbericht) for _ in range(1, 5)]
+
+    await asyncio.gather(*tasks)
