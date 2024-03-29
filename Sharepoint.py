6f5071e2710f304c6085f710f47de20113f0cb4a
jcapobianco <capobianco.uy@gmail.com>
2024-03-29 02:21:40 +0100

    agregado metodo para obtener lista ya formateada a json

--- Sharepoint.py
+++ Sharepoint.py
@@ -1,35 +1,104 @@
 import asyncio
+import json
 from Secret import secrets
-from msgraph import GraphServiceClient
-from azure.identity import ClientSecretCredential
+import requests
 from kiota_abstractions.api_error import APIError
+import msal
 
-# Parametros de API
-credential = ClientSecretCredential(
-    client_secret = secrets.get('CLIENT_SECRET'),
-    client_id = secrets.get('CLIENT_ID'),
-    tenant_id =  secrets.get('TENANT_ID')
-)
-scopes = ['https://graph.microsoft.com/.default']
+# Get token
+client_secret = secrets.get('CLIENT_SECRET')
+client_id = secrets.get('CLIENT_ID')
+tenant_id =  secrets.get('TENANT_ID')
+authority= 'https://login.microsoftonline.com/{}'.format(secrets.get('TENANT_ID'))
+scope = ['https://graph.microsoft.com/.default']
 
-client = GraphServiceClient(credentials=credential, scopes=scopes)
+# Create an MSAL instance providing the client_id, authority and client_credential parameters
+client = msal.ConfidentialClientApplication(client_id, authority=authority, client_credential=client_secret)
 
-usersArray=[]
+# First, try to lookup an access token in cache
+token_result = client.acquire_token_silent(scope, account=None)
+access_token=''
 
-async def get_users():
-    try:
-        users = await client.users.get()
-        if users and users.value:
-            for user in users.value:
-                print(user.id, ",", user.user_principal_name)
-                usersArray.append(user.id)
-        while users is not None and users.odata_next_link is not None:
-            users = await client.users.with_url(users.odata_next_link).get()
-            if users and users.value:
-                for user in users.value:
-                    print(user.id, ",", user.user_principal_name)
-                    usersArray.append(user.id)
-    except APIError as e:
-        print (e.error.message)
+# If the token is available in cache, save it to a variable
+if token_result:
+    access_token = 'Bearer ' + token_result['access_token']
 
+# If the token is not available in cache, acquire a new one from Azure AD and save it to a variable
+if not token_result:
+    token_result = client.acquire_token_for_client(scopes=scope)
+    access_token = 'Bearer ' + token_result['access_token']
 
+async def get_sites():
+    graph_results=[]
+    url= 'https://graph.microsoft.com/v1.0/sites/getAllSites'
+    headers = {
+        'Authorization': access_token,
+        'Content-Type': 'application/json'
+    }
+    while url:
+        try:
+            graph_result = requests.get(url=url, headers=headers).json()
+            graph_results.extend(graph_result['value'])
+            url = graph_result['@odata.nextLink']        
+        except:
+            break
+    return graph_results
+
+async def get_countsites():
+    graph_results=[]
+    url= 'https://graph.microsoft.com/v1.0/sites/getAllSites'
+    headers = {
+        'Authorization': access_token,
+        'Content-Type': 'application/json'
+    }
+    while url:
+        try:
+            graph_result =  requests.get(url=url, headers=headers).json()
+            graph_results.extend(graph_result['value'])
+            url = graph_result['@odata.nextLink']        
+        except:
+            break
+    return len(graph_results)
+
+async def get_siteLists(site):
+    graph_results=[]
+    url= 'https://graph.microsoft.com/v1.0/sites/{}/lists'.format(site)
+    headers = {
+        'Authorization': access_token,
+        'Content-Type': 'application/json'
+    }
+    while url:
+        try:
+            graph_result = requests.get(url=url, headers=headers).json()
+            graph_results.extend(graph_result['value'])
+            url = graph_result['@odata.nextLink']        
+        except:
+            break
+    return graph_results
+
+async def get_listItems(site,list):
+    graph_results=[]
+    listExport=[]
+    
+    url= 'https://graph.microsoft.com/v1.0/sites/{}/lists/{}/items?expand=fields'.format(site,list)
+    headers = {
+        'Authorization': access_token,
+        'Content-Type': 'application/json'
+    }    
+    while url:
+        try:
+            graph_result = requests.get(url=url, headers=headers).json()
+            graph_results.extend(graph_result['value'])
+            url = graph_result['@odata.nextLink']                   
+        except:
+            break
+
+    
+    for x in graph_results:
+        jsonExport={}        
+        for y in x['fields']:
+            jsonExport[y]=x['fields'][y]            
+        listExport.append(jsonExport)
+
+    return listExport
+    #return json.dumps(listExport)
